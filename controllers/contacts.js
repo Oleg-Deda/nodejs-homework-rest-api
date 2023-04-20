4f189acf97510574c3ae76cca9ebcf66309e11a7
Oleg-Deda <dedaoleg1983@gmail.com>
2023-04-20 16:41:27 +0300

    Fix Get and Put error

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -11,13 +11,23 @@ const getById = async (req, res) => {
 	const { contactId } = req.params;
 	const contact = await Contact.findById(contactId);
 	
+	if (!contact) {
+		throw statusError(404, `Contact with ${contactId} not found`);
+	  }
 	res.status(200).json(contact);
 };
 
 const changeContact = async (req, res) => {
-	const contact =await Contact.findByIdAndUpdate(req.params.contactId, req.body, {new: true });
+	if (Object.keys(req.body).length === 0) {
+		throw statusError(400, "Missing fields");
+	  }
+	  const { contactId } = req.params;
 
-	res.status(200).json(contact);
+	const result =await Contact.findByIdAndUpdate(contactId, req.body, {new: true });
+	if (!result) {
+		throw statusError(404, `Contact with ${contactId} not found`);
+	  }
+	res.status(200).json(result);
 };
 
 
@@ -36,9 +46,21 @@ const deleteContact = async (req, res) => {
 };
 
 const updateContactStatus = async (req, res) => {
-	const contact = await Contact.findByIdAndUpdate(req.params.contactId, req.body, {new: true});
-	res.status(200).json(contact);
+	if (Object.keys(req.body).length === 0) {
+		throw statusError(400, "Missing fields");
+	  }
+	  const { contactId } = req.params;
+
+	const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+	if (!result) {
+		throw statusError(404, "Not found");
+	  }
+	res.status(200).json(result);
 };
+
+
+
+
 
 module.exports = {
 	getAllContacts: ctrlWrapper(getAllContacts),
